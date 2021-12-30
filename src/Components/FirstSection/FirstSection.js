e1895f98f6946e94b7b1c3377f40d149f53fa5f4
Chetan Kumar N <chetankumar.nv@gmail.com>
2021-12-30 18:34:24 +0530

    check

--- src/Components/FirstSection/FirstSection.js
+++ src/Components/FirstSection/FirstSection.js
@@ -4,28 +4,30 @@ import { Col, Container, Row } from "react-bootstrap";
 const FirstSection = () => {
   return (
     <div>
-      <div id="first-section" className="pb-3 pb-mb-5">
+      <div id="first-section" className="pb-3 pb-mb-5 bg-dark">
         <Container>
           <Row className="justify-content-center">
             <Col md={6}>
-              <div className="ratio ratio-4x3">
+              <div className="ratio ratio-16x9 mt-5">
                 <iframe
                   src="https://video.wixstatic.com/video/9530b1_374efe2174414ead8a6cc608920f68d9/1080p/mp4/file.mp4"
                   title="YouTube video"
                   allowfullscreen
                 ></iframe>
               </div>
-              <h3>Heading</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor
-                sit amet, consectetur adip Lorem ipsum dolor sit amet,
-                consectetur adip Lorem ipsum dolor sit amet, consectetur adip
-                Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor
-                sit amet, consectetur adip Lorem ipsum dolor sit amet,
-                consectetur adip Lorem ipsum dolor sit amet, consectetur adip
-                Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor
-                sit amet, consectetur adip
-              </p>
+              <div className="">
+                <h3 className="text-white">Heading</h3>
+                <p className="text-white">
+                  Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor
+                  sit amet, consectetur adip Lorem ipsum dolor sit amet,
+                  consectetur adip Lorem ipsum dolor sit amet, consectetur adip
+                  Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor
+                  sit amet, consectetur adip Lorem ipsum dolor sit amet,
+                  consectetur adip Lorem ipsum dolor sit amet, consectetur adip
+                  Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor
+                  sit amet, consectetur adip
+                </p>
+              </div>
               <div className="text-center">
                 <button
                   type="button"
--- src/Components/FirstSection/WhatWeDo.js
+++ src/Components/FirstSection/WhatWeDo.js
@@ -4,16 +4,69 @@ import { Card, Col, Container, Row, Button } from "react-bootstrap";
 
 const WhatWeDo = () => {
   const CardDetails = [
-    { id: 1, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
-    { id: 2, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
-    { id: 3, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
-    { id: 1, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
-    { id: 1, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
-    { id: 1, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
-    { id: 1, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
-    { id: 1, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
-    { id: 1, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
-    { id: 1, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
+    {
+      id: 1,
+      iconurl: "",
+      Title: "Heading",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    },
+    {
+      id: 2,
+      iconurl: "",
+      Title: "Heading",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    },
+    {
+      id: 3,
+      iconurl: "",
+      Title: "Heading",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    },
+    {
+      id: 4,
+      iconurl: "",
+      Title: "Heading",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    },
+    {
+      id: 5,
+      iconurl: "",
+      Title: "Heading",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    },
+    {
+      id: 6,
+      iconurl: "",
+      Title: "Heading",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    },
+    {
+      id: 7,
+      iconurl: "",
+      Title: "Heading",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    },
+    {
+      id: 8,
+      iconurl: "",
+      Title: "Heading",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    },
+    {
+      id: 9,
+      iconurl: "",
+      Title: "Heading",
+      description:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,",
+    },
   ];
   return (
     <div>
