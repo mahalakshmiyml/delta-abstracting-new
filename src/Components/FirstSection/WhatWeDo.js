92999955fec47cd6b68799b0daf08a0012bcdd26
mahalakshmiyml <lakshmiyml@gmail.com>
2021-12-30 18:25:52 +0530

    2 id added

--- src/Components/FirstSection/WhatWeDo.js
+++ src/Components/FirstSection/WhatWeDo.js
@@ -3,7 +3,11 @@ import { Card, Col, Container, Row, Button } from "react-bootstrap";
 // import iconurl from
 
 const WhatWeDo = () => {
-  const CardDetails = [{ id: 1, iconurl: "", Title: "", description: "" }];
+  const CardDetails = [
+    { id: 1, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
+    { id: 2, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
+    { id: 3, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
+  ];
   return (
     <div>
       <div id="what-we-do" className="pt-3 pb-3 pt-md-5 pb-md-5">
@@ -17,9 +21,7 @@ const WhatWeDo = () => {
                   </div>
                   <Card.Body className="text-center">
                     <Card.Title>{item.Title}</Card.Title>
-                    <Card.Text>
-                      {item.description}
-                    </Card.Text>
+                    <Card.Text>{item.description}</Card.Text>
                   </Card.Body>
                 </Card>
               </Col>
