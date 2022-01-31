7ace9cdddd1f1f5fc50ecf2a82f131412aee1a49
Chetan Kumar N <chetankumar.nv@gmail.com>
2022-01-31 11:52:54 +0530

    a

--- src/Components/FirstSection/WhatWeDo.js
+++ src/Components/FirstSection/WhatWeDo.js
@@ -4,18 +4,6 @@ import { Card, Col, Container, Row, Button } from "react-bootstrap";
 
 const WhatWeDo = () => {
   const CardDetails = [
-<<<<<<< HEAD
-    { id: 1, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
-    { id: 2, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
-    { id: 3, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
-    { id: 4, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
-    { id: 5, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
-    { id: 6, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
-    { id: 7, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
-    { id: 1, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
-    { id: 1, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
-    { id: 1, iconurl: "", Title: "TITLE SEARCH", description: "Lorem" },
-=======
     {
       id: 1,
       iconurl: "",
@@ -56,7 +44,6 @@ const WhatWeDo = () => {
         "Title Indexing",
       ],
     },
->>>>>>> e1895f98f6946e94b7b1c3377f40d149f53fa5f4
   ];
   return (
     <div>
